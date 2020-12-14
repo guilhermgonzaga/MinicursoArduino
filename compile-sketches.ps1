fdb77b3258a2964399335f723e64120d0e705d7d
Guilherme Gonzaga de Andrade <guilhermgonzaga@gmail.com>
2020-12-14 15:33:59 -0400

    Atualiza compile-sketches.ps1 e workflow para usar arduino-cli 0.14

--- compile-sketches.ps1
+++ compile-sketches.ps1
@@ -1,6 +1,24 @@
+<#
+.SYNOPSIS
+Check compilation of all sketches in the project.
+.DESCRIPTION
+Check compilation of all sketches in the project.
+No build folder or binaries are copied to the directories.
+An FQBN may be passed as parameter to compile for specific boards.
+A warning level (as used by arduino-cli) may be passed to enable warnings.
+.EXAMPLE
+./compile-sketches.ps1 -warnings default
+.EXAMPLE
+./compile-sketches.ps1 -fqbn arduino:avr:uno
+.LINK
+https://arduino.github.io/arduino-cli/latest/
+.LINK
+https://arduino.github.io/arduino-cli/latest/FAQ/#whats-the-fqbn-string
+#>
+
 Param(
-  [Parameter(ParameterSetName='fqbn')]
-  [string] $fqbn = 'arduino:avr:mega'
+	[string] $fqbn = 'arduino:avr:mega',
+	[string] $warnings = 'all'
 )
 
 $failures = @()
@@ -8,20 +26,18 @@ $failures = @()
 Get-ChildItem -File -Path '[0-9]/sketches/*/*.ino' |
 Where-Object {$_.BaseName -eq $_.Directory.Name} |
 ForEach-Object {
-  $relPath = Resolve-Path -Relative $_.Directory
-  Write-Output "Compiling '$relPath':"
+	$relativePath = Resolve-Path -Relative $_.Directory
+	Write-Output "Compiling '$relativePath':"
 
-  arduino-cli compile --dry-run --fqbn $fqbn "$_"
-  if (-not $?) {
-    $failures += $relPath
-  }
-
-  Write-Output ''
+	arduino-cli compile --fqbn "$fqbn" --warnings "$warnings" "$_"
+	if (-not $?) {
+		$failures += $relativePath
+	}
 }
 
 if ($failures.Count -eq 0) {
-  Write-Output "All sketches compiled successfully."
+	Write-Output "All sketches compiled successfully."
 } else {
-  Write-Output "Compilation failure(s): $($failures.Count)" $failures
-  exit 1
+	Write-Output "Compilation failure(s): $($failures.Count)" $failures
+	exit 1
 }
